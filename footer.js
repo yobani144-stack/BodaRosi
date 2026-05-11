a8e4cfc2828f3afb85adfdd043c4769f43ab1371
yobani144-stack <yobani144@gmail.com>
2026-05-10 18:43:12 -0600

    Update footer.js

--- footer.js
+++ footer.js
@@ -3,3 +3,15 @@ fetch('footer.html')
     .then(data => {
         document.getElementById('contenedor-footer').innerHTML = data;
     });
+// Función para enviar el mensaje personalizado
+function enviarWhatsApp(event) {
+    event.preventDefault();
+    
+    const telefono = "521234567890"; // <-- SUSTITUYE CON TU NÚMERO (Código de país + número)
+    const mensaje = encodeURIComponent("¡Hola! Me gustariapedir mas informacion sobre las invitación! ✨");
+    
+    const url = `https://wa.me/${telefono}?text=${mensaje}`;
+    
+    // Abrir en pestaña nueva
+    window.open(url, '_blank');
+}
