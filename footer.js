e779c1b2cb2583d565eaa886aafc1ac0563efd32
yobani144-stack <yobani144@gmail.com>
2026-05-11 16:59:37 -0600

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,14 +1,9 @@
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('contenedor-footer').innerHTML = data;
-    });
 function pedirInformes(event) {
     // Evitamos que el enlace intente recargar la página
     event.preventDefault();
     
     // Configuración de tu número y mensaje
-    const miTelefono = "527291255411"; // <-- PON AQUÍ TU NÚMERO (Código país + número)
+    const miTelefono = "527191255411"; // <-- PON AQUÍ TU NÚMERO (Código país + número)
     const textoMensaje = "¡Hola! Vi la invitación digital y me encantó su trabajo. ✨ Me gustaría recibir más información y conocer sus paquetes y precios. ¡Muchas gracias!";
     
     // Codificamos el texto para que WhatsApp lo entienda
